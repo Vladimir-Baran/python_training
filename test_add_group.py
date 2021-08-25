134965b51d463e3509a8810e7631853174d5a339
Vladimir-Baran <Allen1Walker@yandex.ru>
2021-08-25 17:27:19 +0300

    каталон запись

--- test_add_group.py
+++ test_add_group.py
@@ -7,13 +7,13 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
-class TestAddGroup(unittest.TestCase):
+class test_add_group(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
+        self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
 
     
-    def test_add_group(self):
+    def test_test_add_group(self):
         wd = self.wd
         wd.get("http://localhost:8080/addressbook/addressbook/")
         wd.find_element_by_name("user").click()
@@ -36,15 +36,15 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_link_text("Logout").click()
     
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
+    # def is_element_present(self, how, what):
+    #     try: self.wd.find_element(by=how, value=what)
+    #     except NoSuchElementException as e: return False
+    #     return True
     
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
+    # def is_alert_present(self):
+    #     try: self.wd.switch_to_alert()
+    #     except NoAlertPresentException as e: return False
+    #     return True
 
     
     def tearDown(self):
