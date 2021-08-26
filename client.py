77e49b29d4272cf0c107a79b0927648c42413fdf
Vladimir-Baran <Allen1Walker@yandex.ru>
2021-08-26 17:04:12 +0300

    Авторизация логна

---
+++ client.py
@@ -0,0 +1,22 @@
+class Login:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+class New_client:
+
+    def __init__(self, firstname, middlename, lastname, nickname, company, address, home_number, bday, bmonth,
+                       byear, group):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.company = company
+        self.address = address
+        self.home_number = home_number
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.group = group
---
+++ test_add_client.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest
+from client import Login
+from client import New_client
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Firefox(firefox_binary=r'C:/Program Files/Mozilla Firefox/firefox.exe')
+        self.wd.implicitly_wait(30)
+
+
+    def test_(self):
+        wd = self.enter_site()
+        self.login(wd, Login(username="admin", password="secret"))
+        self.add_new_client(wd, New_client(firstname="Vladimir", middlename="Baran", lastname="Test", nickname="QA",
+                                           company="Bell", address="Street", home_number="79999999999", bday="1",
+                                           bmonth="April", byear="1991", group="test"))
+        self.back_start_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def back_start_page(self, wd):
+        wd.find_element_by_link_text("home").click()
+
+    def add_new_client(self, wd, client):
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(client.firstname)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(client.middlename)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(client.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(client.nickname)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(client.company)
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(client.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(client.home_number)
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(client.bday)
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(client.bmonth)
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(client.byear)
+        wd.find_element_by_name("new_group").click()
+        Select(wd.find_element_by_name("new_group")).select_by_visible_text(client.group)
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def login(self, wd, login):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(login.username)
+        wd.find_element_by_id("LoginForm").click()
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(login.password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def enter_site(self):
+        wd = self.wd
+        wd.get("http://localhost:8080/addressbook/addressbook/")
+        return wd
+
+    def is_element_present(self, how, what):
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
