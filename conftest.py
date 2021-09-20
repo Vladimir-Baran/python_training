e86d625d55e4a26ae5560feb11b23719f44347c1
Vladimir-Baran <Allen1Walker@yandex.ru>
2021-09-20 13:01:50 +0300

    Задание 8

--- conftest.py
+++ conftest.py
@@ -10,11 +10,12 @@ def app(request):
     global fixture
     if fixture is None:
         fixture = Application()
-        fixture.session.ensure_login(Login(username="admin", password="secret"))
+        # fixture.session.ensure_login(Login(username="admin", password="secret"))
     else:
         if not fixture.is_valid():
             fixture = Application()
-            fixture.session.ensure_login(Login(username="admin", password="secret"))
+            # fixture.session.ensure_login(Login(username="admin", password="secret"))
+    fixture.session.ensure_login(Login(username="admin", password="secret"))
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
