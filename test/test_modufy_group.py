aab8f0438ef70b720ca2d5f7cd57301b08c3fe09
Vladimir-Baran <Allen1Walker@yandex.ru>
2021-10-08 18:49:56 +0300

    Задание 20

--- test/test_modufy_group.py
+++ test/test_modufy_group.py
@@ -3,7 +3,7 @@ import random
 from random import randrange
 
 
-def test_modify_group_name(app, db, check_ui):
+def test_modify_group_header(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
     old_groups = db.get_group_list()
@@ -18,18 +18,18 @@ def test_modify_group_name(app, db, check_ui):
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
-def test_modify_group_header(app, db, check_ui):
+def test_modify_group_name(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
     old_groups = db.get_group_list()
-    index = randrange(len(old_groups))
-    group = Group(header="New header1")
-    group.id = old_groups[index].id
-    app.group.modify_group_by_index(index, group)
+    groups = random.randrange(len(old_groups))
+    group = Group(name="New group1")
+    group.id = old_groups[groups].id
+    app.group.modify_group_by_db(groups.id, group)
     new_groups = db.get_group_list()
     assert len(old_groups) == len(new_groups)
-    # old_groups[index] = group
-    # assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    old_groups[groups] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
